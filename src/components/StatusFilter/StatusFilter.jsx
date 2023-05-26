a611ae17c3884ab9a0b707cc8600e6b25b388546
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-26 03:33:41 +0300

    Update StatusFilter.jsx

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,12 +1,37 @@
+import { useSelector, useDispatch } from 'react-redux';
 import { Button } from 'components/Button/Button';
+import { statusFilters } from 'redux/filters/constants';
+import { getStatusFilter } from 'redux/filters/selectors';
+import { setStatusFilter } from 'redux/filters/actions';
 import css from './StatusFilter.module.css';
 
 export const StatusFilter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getStatusFilter);
+  // Викликаємо генератор екшену та передаємо значення фільтра
+  // Відправляємо результат - екшен зміни фільтра
+  const hendleFilterChange = filter => dispatch(setStatusFilter(filter));
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button
+        selected={filter === statusFilters.all}
+        onClick={() => hendleFilterChange(statusFilters.all)}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === statusFilters.active}
+        onClick={() => hendleFilterChange(statusFilters.active)}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === statusFilters.completed}
+        onClick={() => hendleFilterChange(statusFilters.completed)}
+      >
+        Completed
+      </Button>
     </div>
   );
 };
