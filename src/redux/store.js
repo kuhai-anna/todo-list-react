de16b7ddf5feb3b68fbe5279fa1bd165a2df1181
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-27 01:13:29 +0300

    Add configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,22 @@
 // файл створення стор Redux
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { rootReducer } from './reducer';
+import { configureStore } from '@reduxjs/toolkit';
+import { tasksReducer } from './tasks/tasksSlice';
+import { filtersReducer } from './filters/filtersSlice';
+// import { rootReducer } from './reducer';
+// import { tasksReducer } from './tasks/reducer';
+// import { filtersReducer } from './filters/reducer';
+
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    filters: filtersReducer,
+  },
+});
+
+// -------- before --------
+// import { createStore } from 'redux';
+// import { devToolsEnhancer } from '@redux-devtools/extension';
 
 // Створюємо розширення стора, щоб додати інструменти розробника
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+// const enhancer = devToolsEnhancer();
+// export const store = createStore(rootReducer, enhancer);
