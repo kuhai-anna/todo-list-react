2334aba74392f0b193b0bcf44fde7fd8a4a9a3e3
KuhaiAnna <kuhai.anna@gmail.com>
2023-05-26 03:32:07 +0300

    Create actions.js

---
+++ src/redux/tasks/actions.js
@@ -0,0 +1,27 @@
+// файл оголошення екшенів програми
+import { nanoid } from 'nanoid';
+
+export const addTask = text => {
+  return {
+    type: 'tasks/addTask',
+    payload: {
+      id: nanoid(),
+      completed: false,
+      text,
+    },
+  };
+};
+
+export const deleteTask = taskId => {
+  return {
+    type: 'tasks/deleteTask',
+    payload: taskId,
+  };
+};
+
+export const toggleCompleted = taskId => {
+  return {
+    type: 'tasks/toggleCompleted',
+    payload: taskId,
+  };
+};
