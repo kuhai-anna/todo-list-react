a136deca860abf434983d2923c97dd73747412b6
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-02 22:05:21 +0300

    Add extraReducers

--- src/redux/tasks/tasksSlice.js
+++ src/redux/tasks/tasksSlice.js
@@ -1,45 +1,96 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+import {
+  fetchTasks,
+  addTask,
+  deleteTask,
+  toggleCompleted,
+} from 'redux/operations';
 
-const tasksInitialState = [
-  // { id: 0, text: 'Learn HTML and CSS', completed: true },
-  // { id: 1, text: 'Get good at JavaScript', completed: true },
-  // { id: 2, text: 'Master React', completed: false },
-  // { id: 3, text: 'Discover Redux', completed: false },
-  // { id: 4, text: 'Build amazing apps', completed: false },
-];
+const hendlePending = state => {
+  state.isLoading = true;
+};
+
+const hendleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
 
 const taskSlice = createSlice({
   name: 'tasks',
-  initialState: tasksInitialState,
-  reducers: {
-    addTask: {
-      reducer(state, action) {
-        state.push(action.payload);
-      },
-      prepare(text) {
-        return {
-          payload: {
-            id: nanoid(),
-            completed: false,
-            text,
-          },
-        };
-      },
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
+  // редюсери для 'зовнішніх' типів екшенів
+  extraReducers: {
+    // Виконається в момент старту HTTP-запиту
+    [fetchTasks.pending]: hendlePending,
+    [addTask.pending]: hendlePending,
+    [deleteTask.pending]: hendlePending,
+    [toggleCompleted.pending]: hendlePending,
+    // Виконається якщо HTTP-запит завершився з помилкою
+    [fetchTasks.pending]: hendleRejected,
+    [addTask.pending]: hendleRejected,
+    [deleteTask.pending]: hendleRejected,
+    [toggleCompleted.pending]: hendleRejected,
+    // Виконається якщо HTTP-запит завершився успішно
+    [fetchTasks.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items = action.payload;
     },
-    deleteTask(state, action) {
+    [addTask.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items.push(action.payload);
+    },
+    [deleteTask.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
       const index = state.findIndex(task => task.id === action.payload);
       state.splice(index, 1);
     },
-    toggleCompleted(state, action) {
-      for (const task of state) {
-        if (task.id === action.payload) {
-          task.completed = !task.completed;
-          break;
-        }
-      }
+    [toggleCompleted.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      const index = state.items.findIndex(
+        task => task.id === action.payload.id
+      );
+      state.items.splice(index, 1, action.payload);
     },
   },
+  // reducers: {
+  //   addTask: {
+  //     reducer(state, action) {
+  //       state.push(action.payload);
+  //     },
+  //     prepare(text) {
+  //       return {
+  //         payload: {
+  //           id: nanoid(),
+  //           completed: false,
+  //           text,
+  //         },
+  //       };
+  //     },
+  //   },
+  //   deleteTask(state, action) {
+  //     const index = state.findIndex(task => task.id === action.payload);
+  //     state.splice(index, 1);
+  //   },
+  //   toggleCompleted(state, action) {
+  //     for (const task of state) {
+  //       if (task.id === action.payload) {
+  //         task.completed = !task.completed;
+  //         break;
+  //       }
+  //     }
+  //   },
+  // },
 });
 
-export const { addTask, deleteTask, toggleCompleted } = taskSlice.actions;
+// export const { addTask, deleteTask, toggleCompleted } = taskSlice.actions;
+export const { fetchingnProces, fetchingSucces, fetchingError } =
+  taskSlice.actions;
 export const tasksReducer = taskSlice.reducer;
