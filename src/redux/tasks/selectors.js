5895d0d6499ecc6c12666c35dae9c049f1169d20
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-02 22:05:34 +0300

    Add new selectors

--- src/redux/tasks/selectors.js
+++ src/redux/tasks/selectors.js
@@ -1,2 +1,6 @@
 // файл оголошення функцій-селекторів
 export const getTasks = state => state.tasks;
+
+export const getIsLoading = state => state.tasks.isLoading;
+
+export const getError = state => state.tasks.error;
