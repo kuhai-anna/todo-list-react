b80f61ad514ed4b03c86bcf510aafc24866616ed
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-04 23:26:43 +0300

    Create new selector

--- src/redux/tasks/selectors.js
+++ src/redux/tasks/selectors.js
@@ -1,5 +1,23 @@
-// файл оголошення функцій-селекторів
-export const getTasks = state => state.tasks;
+import { statusFilters } from 'redux/filters/constants';
+import { selectStatusFilter } from 'redux/filters/selectors';
+
+// "атомарний" селектор (повертає честину стану без обчислень)
+export const selectTasks = state => state.tasks;
+
+// "складовий" селектор (повертає значення, що обчислюються)
+export const selectVisibleTasks = state => {
+  const tasks = selectTasks(state);
+  const statusFilter = selectStatusFilter(state);
+
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
 
 // -------- код для використання бази даних
 // export const getIsLoading = state => state.tasks.isLoading;
