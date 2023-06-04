58bb05a049a0181b8509ce8ca3afeb58c8c369c5
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-04 23:52:43 +0300

    Delete TaskCounter func

--- src/components/TaskCounter/TaskCounter.jsx
+++ src/components/TaskCounter/TaskCounter.jsx
@@ -1,21 +1,9 @@
 import { useSelector } from 'react-redux';
-import { selectTasks } from 'redux/tasks/selectors';
+import { selectTaskCount } from 'redux/tasks/selectors';
 import css from './TaskCounter.module.css';
 
 export const TaskCounter = () => {
-  const tasks = useSelector(selectTasks);
-
-  const count = tasks.reduce(
-    (acc, task) => {
-      if (task.completed) {
-        acc.completed += 1;
-      } else {
-        acc.active += 1;
-      }
-      return acc;
-    },
-    { completed: 0, active: 0 }
-  );
+  const count = useSelector(selectTaskCount);
 
   return (
     <div>
