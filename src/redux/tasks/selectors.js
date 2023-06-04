6de3ee908bd632c8a7564ecfabd0f3146a2d69e5
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-04 23:52:23 +0300

    Add createSelector

--- src/redux/tasks/selectors.js
+++ src/redux/tasks/selectors.js
@@ -1,23 +1,43 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { statusFilters } from 'redux/filters/constants';
 import { selectStatusFilter } from 'redux/filters/selectors';
 
 // "атомарний" селектор (повертає честину стану без обчислень)
 export const selectTasks = state => state.tasks;
 
-// "складовий" селектор (повертає значення, що обчислюються)
-export const selectVisibleTasks = state => {
-  const tasks = selectTasks(state);
-  const statusFilter = selectStatusFilter(state);
+// мемоізований "складовий" селектор (повертає значення, що обчислюються)
+export const selectVisibleTasks = createSelector(
+  [selectTasks, selectStatusFilter],
+  (tasks, statusFilter) => {
+    console.log('Calculating visible tasks. Now memoized!');
 
-  switch (statusFilter) {
-    case statusFilters.active:
-      return tasks.filter(task => !task.completed);
-    case statusFilters.completed:
-      return tasks.filter(task => task.completed);
-    default:
-      return tasks;
+    switch (statusFilter) {
+      case statusFilters.active:
+        return tasks.filter(task => !task.completed);
+      case statusFilters.completed:
+        return tasks.filter(task => task.completed);
+      default:
+        return tasks;
+    }
   }
-};
+);
+
+// мемоізований "складовий" селектор
+export const selectTaskCount = createSelector([selectTasks], tasks => {
+  console.log('Calculating count tasks. Now memoized!');
+
+  return tasks.reduce(
+    (count, task) => {
+      if (task.completed) {
+        count.completed += 1;
+      } else {
+        count.active += 1;
+      }
+      return count;
+    },
+    { completed: 0, active: 0 }
+  );
+});
 
 // -------- код для використання бази даних
 // export const getIsLoading = state => state.tasks.isLoading;
