afabd7fc9d6225e127bf2aea71e4e2b2ea2e45cf
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-04 23:28:19 +0300

    Delete getVisibleTasks func

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,32 +1,14 @@
 import { useSelector } from 'react-redux';
 import { Task } from 'components/Task/Task';
-import { statusFilters } from 'redux/filters/constants';
-import { getStatusFilter } from 'redux/filters/selectors';
-import { getTasks } from 'redux/tasks/selectors';
+import { selectVisibleTasks } from 'redux/tasks/selectors';
 import css from './TaskList.module.css';
 
-const getVisibleTasks = (tasks, statusFilter) => {
-  switch (statusFilter) {
-    case statusFilters.active:
-      return tasks.filter(task => !task.completed);
-    case statusFilters.completed:
-      return tasks.filter(task => task.completed);
-    default:
-      return tasks;
-  }
-};
-
 export const TaskList = () => {
-  // Отримуємо масив завдань із стану Redux
-  const tasks = useSelector(getTasks);
-  // Отримуємо значення фільтра із стану Redux
-  const statusFilter = useSelector(getStatusFilter);
-  // Обчислюємо масив завдань, які необхідно відображати в інтерфейсі
-  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+  const tasks = useSelector(selectVisibleTasks);
 
   return (
     <ul className={css.list}>
-      {visibleTasks.map(task => (
+      {tasks.map(task => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
