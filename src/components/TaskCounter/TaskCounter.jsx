afec4c49872da6b24ed47e77d77c58032a444123
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-10 03:15:24 +0300

    Update TaskCounter.jsx

--- src/components/TaskCounter/TaskCounter.jsx
+++ src/components/TaskCounter/TaskCounter.jsx
@@ -6,7 +6,8 @@ export const TaskCounter = () => {
   const count = useSelector(selectTaskCount);
 
   return (
-    <div>
+    <div className={css.container}>
+      <h2 className={css.title}>Tasks</h2>
       <p className={css.text}>Active: {count.active}</p>
       <p className={css.text}>Completed: {count.completed}</p>
     </div>
