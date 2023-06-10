1d14468b0af34aed23d3d7d1859e5af9b175c381
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-10 03:15:54 +0300

    Add TaskCounter and StatusFilter

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,13 +1,15 @@
 // -------- код для використання бази даних
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { fetchTasks } from 'redux/operations';
+import { fetchTasks } from 'redux/tasks/operations';
 
 import { Helmet } from 'react-helmet';
 import { TaskForm } from 'components/TaskForm/TaskForm';
 import { TaskList } from 'components/TaskList/TaskList';
 // -------- код для використання бази даних
 import { getError, getIsLoading } from 'redux/tasks/selectors';
+import { TaskCounter } from 'components/TaskCounter/TaskCounter';
+import { StatusFilter } from 'components/StatusFilter/StatusFilter';
 
 const Tasks = () => {
   // -------- код для використання бази даних
@@ -24,6 +26,11 @@ const Tasks = () => {
       <Helmet>
         <title>Your tasks</title>
       </Helmet>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <TaskCounter />
+        <StatusFilter />
+      </div>
+
       <TaskForm />
       {isLoading && !error && <b>Request in progress...</b>}
       <TaskList />
