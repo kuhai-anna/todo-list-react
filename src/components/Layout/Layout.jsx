8bd15b84d377a2b547792a0b4bead1272f460921
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-10 03:15:03 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { Suspense } from 'react';
+import { AppBar } from 'components/AppBar/AppBar';
 import css from './Layout.module.css';
 
-export const Layout = ({ children }) => {
-  return <main className={css.container}>{children}</main>;
+export const Layout = () => {
+  return (
+    <div className={css.container}>
+      <AppBar />
+      <Suspense fallback={null}>
+        <Outlet />
+      </Suspense>
+      <Toaster position="top-right" reverseOrder={false} />
+    </div>
+  );
 };
