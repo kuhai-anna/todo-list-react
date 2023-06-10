252e641e5d187750098f70e76314b5ea9775e06b
KuhaiAnna <kuhai.anna@gmail.com>
2023-06-10 03:14:07 +0300

    Create Navigation.jsx

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,17 @@
+import { NavLink } from 'react-router-dom';
+import { useAuth } from 'hooks';
+import css from './Navigation.module.css';
+
+export const Navigation = () => {
+  const { isLoggedIn } = useAuth();
+  return (
+    <div className={css.link} to="/">
+      <Navigation>Home</Navigation>
+      {isLoggedIn && (
+        <NavLink className={css.link} to="/tasks">
+          Tasks
+        </NavLink>
+      )}
+    </div>
+  );
+};
